e05ace8c1b08c18b00f5b2637f5221cbbb0c3ce4
ICEPINK <venayoda@gmail.com>
2024-03-13 15:17:25 +0100

    Render triangle

--- src/cpu_renderer.cpp
+++ src/cpu_renderer.cpp
@@ -3,20 +3,22 @@
 #include <algorithm>
 #include <chrono>
 
+#include <glm/ext.hpp>
+
 namespace Vis {
 
 Solid CpuRenderer::parse_obj(std::string object_string) {
     Solid solid{
         object_string,
         glm::mat4{},
-        {{Topology::Line, 0, 4}},
+        {{Topology::Triangle, 0, 2}},
         {
-            Vertex({-0.5, -0.5, 0.0, 1.0}, {1.0, 0.0, 0.0, 1.0}, {0.0, 0.0}),
-            Vertex({0.5, -0.5, 0.0, 1.0}, {0.0, 1.0, 0.0, 1.0}, {1.0, 0.0}),
-            Vertex({-0.5, 0.5, 0.0, 1.0}, {0.0, 0.0, 1.0, 1.0}, {0.0, 1.0}),
-            Vertex({0.5, 0.5, 0.0, 1.0}, {1.0, 1.0, 1.0, 1.0}, {1.0, 1.0}),
+            Vertex({-0.5, -0.5, -1.0, 1.0}, {1.0, 0.0, 0.0, 1.0}, {0.0, 0.0}),
+            Vertex({0.5, -0.5, -1.0, 1.0}, {0.0, 1.0, 0.0, 1.0}, {1.0, 0.0}),
+            Vertex({-0.5, 0.5, -1.0, 1.0}, {0.0, 0.0, 1.0, 1.0}, {0.0, 1.0}),
+            Vertex({0.5, 0.5, -1.0, 1.0}, {1.0, 0.0, 0.0, 1.0}, {1.0, 1.0}),
         },
-        {0, 1, 1, 3, 3, 2, 2, 0}};
+        {0, 1, 2, 1, 2, 3}};
     return solid;
 }
 
@@ -27,12 +29,26 @@ void CpuRenderer::trasform_to_screen(Vertex &vertex) {
     y = (y + 1) / 2.0 * m_image->get_height();
 }
 
-// void rasterize_point(Vertex a) {}
+void CpuRenderer::set_pixel(int64_t x, int64_t y, Vertex &vertex) {
+    if (x < 0 || static_cast<size_t>(x) > m_width) {
+        return;
+    }
+    if (y < 0 || static_cast<size_t>(y) > m_height) {
+        return;
+    }
+
+    if (vertex.position.z < m_depth_buffer->get_depth(x, y)) {
+        m_depth_buffer->set_depth(x, y, vertex.position.z);
+        m_image->set_pixel(x, y, vertex.color);
+    }
+}
+
 void CpuRenderer::rasterize_line(Vertex &a, Vertex &b) {
+    // DDA
     trasform_to_screen(a);
     trasform_to_screen(b);
 
-    ColorRGBA32f color = a.color;
+    Vertex vertex_atr = a;
 
     float alpha = (b.position.y - a.position.y) / (b.position.x - a.position.x);
 
@@ -43,8 +59,8 @@ void CpuRenderer::rasterize_line(Vertex &a, Vertex &b) {
 
         alpha = (b.position.x - a.position.x) / (b.position.y - a.position.y);
         float x = a.position.x;
-        for (size_t y = a.position.y; y <= b.position.y; ++y) {
-            m_image->set_pixel(x, y, color);
+        for (int y = a.position.y; y <= b.position.y; ++y) {
+            set_pixel(x, y, vertex_atr);
             x += alpha;
         }
     } else {
@@ -53,13 +69,94 @@ void CpuRenderer::rasterize_line(Vertex &a, Vertex &b) {
         }
 
         float y = a.position.y;
-        for (size_t x = a.position.x; x <= b.position.x; ++x) {
-            m_image->set_pixel(x, y, color);
+        for (int x = a.position.x; x <= b.position.x; ++x) {
+            set_pixel(x, y, vertex_atr);
             y += alpha;
         }
     }
 }
-// void rasterize_triangle(Vertex a, Vertex b, Vertex c) {}
+
+void CpuRenderer::render_triangle(Vertex &a, Vertex &b, Vertex &c) {
+    std::vector<Vertex> vertices = {a, b, c};
+
+    // clip_before_dehomog();
+
+    // dehomog
+    std::for_each(vertices.begin(), vertices.end(), [](Vertex &vertex) {
+        vertex = vertex * (1.0 / vertex.position.w);
+    });
+
+    // clip_after_dehomog();
+
+    for (size_t i = 0; i < vertices.size(); i += 3) {
+        rasterize_triangle(vertices[i], vertices[i + 1], vertices[i + 2]);
+    }
+}
+
+void CpuRenderer::rasterize_triangle(Vertex &a, Vertex &b, Vertex &c) {
+    trasform_to_screen(a);
+    trasform_to_screen(b);
+    trasform_to_screen(c);
+
+    // a..y < b..y < c..y
+    if (a.position.y > b.position.y) {
+        std::swap(a, b);
+    }
+    if (b.position.y > c.position.y) {
+        std::swap(b, c);
+    }
+    if (a.position.y > b.position.y) {
+        std::swap(a, b);
+    }
+
+    for (size_t y = std::max(a.position.y, 0.0f);
+         y < std::min(b.position.y, static_cast<float>(m_height)); ++y) {
+
+        float ab_t = (y - a.position.y) / (b.position.y - a.position.y);
+        Vertex ab = Vertex::interpolate(ab_t, a, b);
+
+        float ac_t = (y - a.position.y) / (c.position.y - a.position.y);
+        Vertex ac = Vertex::interpolate(ac_t, a, c);
+
+        if (ab.position.x > ac.position.x) {
+            std::swap(ab, ac);
+        }
+
+        for (size_t x = std::max(ab.position.x, 0.0f);
+             x <= std::min(ac.position.x, static_cast<float>(m_width)); ++x) {
+
+            float ab_ac_t =
+                (x - ab.position.x) / (ac.position.x - ab.position.x);
+            Vertex ab_ac = Vertex::interpolate(ab_ac_t, ab, ac);
+
+            set_pixel(x, y, ab_ac);
+        }
+    }
+
+    for (size_t y = std::max(b.position.y, 0.0f);
+         y <= std::min(c.position.y, static_cast<float>(m_height)); ++y) {
+
+        float bc_t = (y - b.position.y) / (c.position.y - b.position.y);
+        Vertex bc = Vertex::interpolate(bc_t, b, c);
+
+        float ac_t = (y - a.position.y) / (c.position.y - a.position.y);
+        Vertex ac = Vertex::interpolate(ac_t, a, c);
+
+        if (bc.position.x > ac.position.x) {
+            std::swap(bc, ac);
+        }
+
+        for (size_t x = std::max(bc.position.x, 0.0f);
+             x <= std::min(ac.position.x, static_cast<float>(m_width)); ++x) {
+
+            float bc_ac_t =
+                (x - bc.position.x) / (ac.position.x - bc.position.x);
+            Vertex bc_ac = Vertex::interpolate(bc_ac_t, bc, ac);
+
+            set_pixel(x, y, bc_ac);
+        }
+    }
+}
 
 void CpuRenderer::render_line(Vertex &a, Vertex &b) {
     std::vector<Vertex> vertices = {a, b};
@@ -68,7 +165,7 @@ void CpuRenderer::render_line(Vertex &a, Vertex &b) {
 
     // dehomog
     std::for_each(vertices.begin(), vertices.end(), [](Vertex &vertex) {
-        vertex = vertex * (1 / vertex.position.w);
+        vertex = vertex * (1.0 / vertex.position.w);
     });
 
     // clip_after_dehomog();
@@ -79,9 +176,12 @@ void CpuRenderer::render_line(Vertex &a, Vertex &b) {
 }
 
 void CpuRenderer::render_solid(Solid &solid) {
+
     glm::mat4 model_matrix{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
     Camera camera{{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}, {}, {}, {}};
     glm::mat4 projection_matrix{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
+    projection_matrix = glm::perspective(
+        3.1415f / 2.0f, m_width / static_cast<float>(m_height), 1.0f, 10.0f);
 
     glm::mat4 matrix = projection_matrix * camera.view_matrix * model_matrix;
 
@@ -101,13 +201,25 @@ void CpuRenderer::render_solid(Solid &solid) {
             break;
         case Topology::Line:
             for (size_t i = 0; i < layout.count; ++i) {
-                render_line(
-                    transformed_vertices[solid.indices[layout.start + (i * 2)]],
-                    transformed_vertices[solid.indices[layout.start + (i * 2) +
-                                                       1]]);
+                size_t &index_a = solid.indices[layout.start + (i * 2)];
+                size_t &index_b = solid.indices[layout.start + (i * 2) + 1];
+                Vertex &vertex_a = transformed_vertices[index_a];
+                Vertex &vertex_b = transformed_vertices[index_b];
+
+                render_line(vertex_a, vertex_b);
             }
             break;
         case Topology::Triangle:
+            for (size_t i = 0; i < layout.count; ++i) {
+                size_t &index_a = solid.indices[layout.start + (i * 3)];
+                size_t &index_b = solid.indices[layout.start + (i * 3) + 1];
+                size_t &index_c = solid.indices[layout.start + (i * 3) + 2];
+                Vertex &vertex_a = transformed_vertices[index_a];
+                Vertex &vertex_b = transformed_vertices[index_b];
+                Vertex &vertex_c = transformed_vertices[index_c];
+
+                render_triangle(vertex_a, vertex_b, vertex_c);
+            }
             break;
         }
     }
@@ -124,6 +236,7 @@ void *CpuRenderer::render_image(const size_t width, const size_t height) {
 
     if (m_width != width || m_height != height) {
         m_image = std::make_unique<Image>(width, height);
+        m_depth_buffer = std::make_unique<DepthBuffer>(width, height);
         m_width = width;
         m_height = height;
     }
@@ -133,6 +246,8 @@ void *CpuRenderer::render_image(const size_t width, const size_t height) {
     auto start = std::chrono::high_resolution_clock::now();
 
     m_image->clear(m_scene_info_ref.clear_color);
+    m_depth_buffer->clear(1.0);
+
     render_solid(solid);
 
     auto end = std::chrono::high_resolution_clock::now();
--- src/cpu_renderer.hpp
+++ src/cpu_renderer.hpp
@@ -4,6 +4,7 @@
 
 #include <glm/glm.hpp>
 
+#include "depth_buffer.hpp"
 #include "image.hpp"
 
 namespace Vis {
@@ -21,31 +22,35 @@ struct Vertex {
     glm::vec2 uv;
     float one{1.0};
 
-    Vertex operator+(Vertex &vertex) {
+    Vertex operator+(const Vertex &vertex) const {
         return {position + vertex.position, color + vertex.color,
                 uv + vertex.uv, one + vertex.one};
     }
 
-    Vertex operator+(float f) {
+    Vertex operator+(const float f) const {
         return {position + f, color + f, uv + f, one + f};
     }
 
-    Vertex operator-(Vertex &vertex) {
+    Vertex operator-(const Vertex &vertex) const {
         return {position - vertex.position, color - vertex.color,
                 uv - vertex.uv, one - vertex.one};
     }
 
-    Vertex operator-(float f) {
+    Vertex operator-(const float f) const {
         return {position - f, color - f, uv - f, one - f};
     }
 
-    Vertex operator*(Vertex &vertex) {
+    Vertex operator*(const Vertex &vertex) const {
         return {position * vertex.position, color * vertex.color,
                 uv * vertex.uv, one * vertex.one};
     }
 
-    Vertex operator*(float f) {
+    Vertex operator*(const float f) const {
         return {position * f, color * f, uv * f, one * f};
+    }
+
+    static Vertex interpolate(const float t, const Vertex &a, const Vertex &b) {
+        return (a * (1 - t)) + (b * t);
     }
 };
 
@@ -71,16 +76,24 @@ class CpuRenderer {
     ~CpuRenderer();
 
     void *render_image(const size_t width, const size_t height);
+
+  private:
     void render_solid(Solid &solid);
+    // void render_point(Vertex &a);
     void render_line(Vertex &a, Vertex &b);
+    void render_triangle(Vertex &a, Vertex &b, Vertex &c);
+    // void rasterize_point(Vertex &a);
     void rasterize_line(Vertex &a, Vertex &b);
+    void rasterize_triangle(Vertex &a, Vertex &b, Vertex &c);
     void trasform_to_screen(Vertex &vertex);
     Solid parse_obj(std::string object_string);
+    void set_pixel(int64_t x, int64_t y, Vertex &vertex);
 
   private:
     size_t m_width{0};
     size_t m_height{0};
     std::unique_ptr<Image> m_image;
+    std::unique_ptr<DepthBuffer> m_depth_buffer;
     SceneInfo &m_scene_info_ref;
 };
 
