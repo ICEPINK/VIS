7eacac23ae9aaac06717830affc29f10d4545318
ICEPINK <venayoda@gmail.com>
2024-03-29 16:25:22 +0100

    Keybinds for simulation camera

--- src/window.cpp
+++ src/window.cpp
@@ -80,100 +80,202 @@ void Window::handle_input([[maybe_unused]] AppInfo &app_info,
     }
 
     if (app_info.alt_mode) {
-        double new_mouse_x;
-        double new_mouse_y;
-        glfwGetCursorPos(m_window_ptr, &new_mouse_x, &new_mouse_y);
+        if (scene_info.simulated_camera) {
+            double new_mouse_x;
+            double new_mouse_y;
+            glfwGetCursorPos(m_window_ptr, &new_mouse_x, &new_mouse_y);
 
-        if (scene_info.camera) {
-            float d_mouse_x =
-                static_cast<float>(new_mouse_x - app_info.mouse_x);
-            constexpr float angel = -glm::pi<float>() / 180.0f;
-            scene_info.camera->rotate_horizon(d_mouse_x * angel * 0.1f);
-        }
-
-        if (scene_info.camera) {
-            float d_mouse_y =
-                static_cast<float>(new_mouse_y - app_info.mouse_y);
-            constexpr float angel = glm::pi<float>() / 180.0f;
-            scene_info.camera->rotate_vertical(d_mouse_y * angel * 0.1f);
-        }
-
-        app_info.mouse_x = 0;
-        app_info.mouse_y = 0;
-        glfwSetCursorPos(m_window_ptr, app_info.mouse_x, app_info.mouse_y);
-
-        int key;
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_W);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_forward(0.02f);
+            if (scene_info.simulated_camera) {
+                float d_mouse_x =
+                    static_cast<float>(new_mouse_x - app_info.mouse_x);
+                constexpr float angel = -glm::pi<float>() / 180.0f;
+                scene_info.simulated_camera->rotate_horizon(d_mouse_x * angel *
+                                                            0.1f);
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_S);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_backward(0.02f);
+            if (scene_info.simulated_camera) {
+                float d_mouse_y =
+                    static_cast<float>(new_mouse_y - app_info.mouse_y);
+                constexpr float angel = glm::pi<float>() / 180.0f;
+                scene_info.simulated_camera->rotate_vertical(d_mouse_y * angel *
+                                                             0.1f);
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_A);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_left(0.02f);
+            app_info.mouse_x = 0;
+            app_info.mouse_y = 0;
+            glfwSetCursorPos(m_window_ptr, app_info.mouse_x, app_info.mouse_y);
+
+            int key;
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_W);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_forward(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_D);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_right(0.02f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_S);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_backward(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_SPACE);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_up(0.02f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_A);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_left(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_LEFT_CONTROL);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->move_down(0.02f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_D);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_right(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_Q);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->rotate_horizon(glm::pi<float>() / 180 *
-                                                  0.5f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_SPACE);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_up(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_E);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->rotate_horizon(-glm::pi<float>() / 180 *
-                                                  0.5f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_LEFT_CONTROL);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->move_down(0.02f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_Y);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
-            if (scene_info.camera) {
-                scene_info.camera->rotate_vertical(-glm::pi<float>() / 180 *
-                                                   0.5f);
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Q);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->rotate_horizon(
+                        glm::pi<float>() / 180 * 0.5f);
+                }
             }
-        }
 
-        key = glfwGetKey(m_window_ptr, GLFW_KEY_Z);
-        if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_E);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->rotate_horizon(
+                        -glm::pi<float>() / 180 * 0.5f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Y);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->rotate_vertical(
+                        -glm::pi<float>() / 180 * 0.5f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Z);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.simulated_camera) {
+                    scene_info.simulated_camera->rotate_vertical(
+                        glm::pi<float>() / 180 * 0.5f);
+                }
+            }
+        } else {
+
+            double new_mouse_x;
+            double new_mouse_y;
+            glfwGetCursorPos(m_window_ptr, &new_mouse_x, &new_mouse_y);
+
             if (scene_info.camera) {
-                scene_info.camera->rotate_vertical(glm::pi<float>() / 180 *
-                                                   0.5f);
+                float d_mouse_x =
+                    static_cast<float>(new_mouse_x - app_info.mouse_x);
+                constexpr float angel = -glm::pi<float>() / 180.0f;
+                scene_info.camera->rotate_horizon(d_mouse_x * angel * 0.1f);
+            }
+
+            if (scene_info.camera) {
+                float d_mouse_y =
+                    static_cast<float>(new_mouse_y - app_info.mouse_y);
+                constexpr float angel = glm::pi<float>() / 180.0f;
+                scene_info.camera->rotate_vertical(d_mouse_y * angel * 0.1f);
+            }
+
+            app_info.mouse_x = 0;
+            app_info.mouse_y = 0;
+            glfwSetCursorPos(m_window_ptr, app_info.mouse_x, app_info.mouse_y);
+
+            int key;
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_W);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_forward(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_S);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_backward(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_A);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_left(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_D);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_right(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_SPACE);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_up(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_LEFT_CONTROL);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->move_down(0.02f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Q);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->rotate_horizon(glm::pi<float>() / 180 *
+                                                      0.5f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_E);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->rotate_horizon(-glm::pi<float>() / 180 *
+                                                      0.5f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Y);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->rotate_vertical(-glm::pi<float>() / 180 *
+                                                       0.5f);
+                }
+            }
+
+            key = glfwGetKey(m_window_ptr, GLFW_KEY_Z);
+            if (key == GLFW_PRESS || key == GLFW_REPEAT) {
+                if (scene_info.camera) {
+                    scene_info.camera->rotate_vertical(glm::pi<float>() / 180 *
+                                                       0.5f);
+                }
             }
         }
     }
