2df72c88eb79d25401344ac0db6b308d06581098
ICEPINK <venayoda@gmail.com>
2024-04-16 15:19:28 +0200

    run script print and cmake variables

--- run.sh
+++ run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
+echo "-> CMAKE SETUP" &&
 cmake \
     -S ./ \
     -B ./build/ \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DCMAKE_C_COMPILER=gcc \
     &&
+echo "" &&
+echo "-> CMAKE BUILD" &&
 cmake --build ./build &&
+echo "" &&
+echo "-> RUN" &&
 ./build/VIS
