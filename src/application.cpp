c3b38cd6f19a679d0ea126a8ddaef93ed38579be
ICEPINK <venayoda@gmail.com>
2024-04-18 12:59:12 +0200

    Testing new classes

--- src/application.cpp
+++ src/application.cpp
@@ -1,5 +1,8 @@
 #include "application.hpp"
 
+#include "glad.hpp"
+#include "window.hpp"
+
 #include <iostream>
 
 namespace Vis {
@@ -7,6 +10,27 @@ namespace Vis {
 Application::Application(const std::vector<std::string_view> &args) {
     if (handle_args(args)) {
         return;
+    }
+
+    auto glfw = std::make_shared<Glfw>();
+    // const auto glsl_version = "#version 460";
+    glfw->window_hint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfw->window_hint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfw->window_hint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfw->window_hint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+
+    Window window({800, 600, "VIS"}, glfw);
+    window.make_context_current();
+
+    Glad::load_gl_loader((GLADloadproc)glfw->get_proc_address());
+    glDebugMessageCallback(Glad::print_gl_message, nullptr);
+    std::cout << "OpenGL: version " << glGetString(GL_VERSION) << '\n';
+
+    while (!window.should_close()) {
+        glfw->poll_events();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_LINES, 0, 3);
+        window.swap_buffers();
     }
 }
 
