a992726340daecff4cca83695107ce33830d2607
ICEPINK <venayoda@gmail.com>
2024-05-07 12:51:31 +0200

    depth bufffer added

--- src/image.cpp
+++ src/image.cpp
@@ -4,18 +4,23 @@ namespace Vis {
 Image::Image() {}
 Image::Image(const size_t width, const size_t height)
     : m_width(width), m_height(height) {
-  m_buffer.resize(width * height);
+  m_color_buffer.resize(width * height);
+  m_depth_buffer.resize(width * height);
 }
 
 auto Image::resize(const size_t width, const size_t height) -> void {
   m_width = width;
   m_height = height;
-  m_buffer.resize(width * height);
+  m_color_buffer.resize(width * height);
+  m_depth_buffer.resize(width * height);
 }
 
-auto Image::clear(const glm::dvec4 &color) -> void {
-  for (auto &pixel : m_buffer) {
+auto Image::clear(const glm::dvec4 &color, const double depth) -> void {
+  for (auto &pixel : m_color_buffer) {
     pixel = dvec4_to_rgba8(color);
+  }
+  for (auto &value : m_depth_buffer) {
+    value = depth;
   }
 }
 
@@ -24,17 +29,35 @@ auto Image::set_pixel(const size_t x, const size_t y,
   if (x >= m_width || y >= m_height) {
     return;
   }
-  m_buffer[x + y * m_width] = dvec4_to_rgba8(color);
+  m_color_buffer[x + y * m_width] = dvec4_to_rgba8(color);
+}
+auto Image::set_depth(const size_t x, const size_t y,
+                      const double depth) -> void {
+  if (x >= m_width || y >= m_height) {
+    return;
+  }
+  m_depth_buffer[x + y * m_width] = depth;
 }
 
 [[nodiscard]] auto Image::get_width() const -> size_t { return m_width; }
 [[nodiscard]] auto Image::get_height() const -> size_t { return m_height; }
 [[nodiscard]] auto Image::get_image_data() -> ColorRGBA8 * {
-  return m_buffer.data();
+  return m_color_buffer.data();
 }
 [[nodiscard]] auto Image::get_pixel(const size_t x,
                                     const size_t y) const -> glm::dvec4 {
-  return rgba8_to_dvec4(m_buffer[x + y * m_width]);
+  if (x >= m_width || y >= m_height) {
+    auto min_double = std::numeric_limits<double>::min();
+    return {min_double, min_double, min_double, min_double};
+  }
+  return rgba8_to_dvec4(m_color_buffer[x + y * m_width]);
+}
+[[nodiscard]] auto Image::get_depth(const size_t x,
+                                    const size_t y) const -> double {
+  if (x >= m_width || y >= m_height) {
+    return std::numeric_limits<double>::min();
+  }
+  return m_depth_buffer[x + y * m_width];
 }
 
 auto Image::dvec4_to_rgba8(const glm::dvec4 &color) const -> ColorRGBA8 {
--- src/image.hpp
+++ src/image.hpp
@@ -22,16 +22,19 @@ public:
 
   auto resize(const size_t width, const size_t height) -> void;
 
-  auto clear(const glm::dvec4 &color) -> void;
+  auto clear(const glm::dvec4 &color = {0.0, 0.0, 0.0, 1.0},
+             const double depth = 1.0) -> void;
 
   auto set_pixel(const size_t x, const size_t y,
                  const glm::dvec4 &color) -> void;
+  auto set_depth(const size_t x, const size_t y, const double depth) -> void;
 
   [[nodiscard]] auto get_width() const -> size_t;
   [[nodiscard]] auto get_height() const -> size_t;
   [[nodiscard]] auto get_image_data() -> ColorRGBA8 *;
   [[nodiscard]] auto get_pixel(const size_t x,
                                const size_t y) const -> glm::dvec4;
+  [[nodiscard]] auto get_depth(const size_t x, const size_t y) const -> double;
 
 private:
   auto dvec4_to_rgba8(const glm::dvec4 &color) const -> ColorRGBA8;
@@ -41,7 +44,8 @@ private:
 private:
   size_t m_width{0};
   size_t m_height{0};
-  std::vector<ColorRGBA8> m_buffer;
+  std::vector<ColorRGBA8> m_color_buffer;
+  std::vector<double> m_depth_buffer;
 };
 
 } // namespace Vis
