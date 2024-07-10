33662588b018a86e129271a9123d598f7358696d
ICEPINK <venayoda@gmail.com>
2024-07-10 11:23:55 +0200

    update run.lua

--- .nvim/run.lua
+++ .nvim/run.lua
@@ -1,79 +1,49 @@
--- .nvim/run.lua ---------------------------------------------------------------
--- Globals ---------------------------------------------------------------------
-local g_build_type = 'Release'
-local g_executable = 'start cmd /C .\\build\\VIS.exe'
+-- .nvim/run.lua --
+
+-- global variables --
+local g_executable = 'VIS.exe'
 local g_executable_args = {}
-local g_cmake_cxx_compiler = 'clang++'
-local g_cmake_c_compiler = 'clang'
--- FncDefinitions --------------------------------------------------------------
+local g_setup = 'cmake'
+local g_setup_args = {} 
+local g_setup_build_dir = 'build\\' 
+local g_setup_build_type = 'Release'
+local g_setup_c_compiler = 'clang'
+local g_setup_cxx_compiler = 'clang++'
+local g_setup_generator = 'Ninja' 
+local g_setup_source_dir = '.\\' 
+
+-- fuctions --
 function in_args(a)
     for i,v in ipairs(arg) do
-        if v == a then 
-            return true
-        end
+        if v == a then return true end
     end
     return false
 end
-function to_cmd(arr)
-    local result_cmd = ''
-    for i,v in ipairs(arr) do
-        result_cmd = result_cmd .. v .. ' '
-    end
-    return result_cmd
+local function setup()
+    print('@setup(' .. g_setup .. ')')
+    return os.execute(g_setup
+    .. ' -G "' .. g_setup_generator .. '"'
+    .. ' -S ' .. g_setup_source_dir
+    .. ' -B ' .. g_setup_build_dir
+    .. ' -DCMAKE_BUILD_TYPE=' .. g_setup_build_type
+    .. ' -DCMAKE_CXX_COMPILER=' .. g_setup_cxx_compiler
+    .. ' -DCMAKE_C_COMPILER=' .. g_setup_c_compiler
+    .. ' ' .. table.concat(g_setup_args, ' ')
+    )
 end
-function setup()
-    print('@setup(cmake)')
-    setup_cmd = {
-        'cmake',
-        '-G "Ninja"',
-        '-S ./',
-        '-B ./build/',
-        '-DCMAKE_BUILD_TYPE=' .. g_build_type,
-        '-DCMAKE_CXX_COMPILER=' .. g_cmake_cxx_compiler,
-        '-DCMAKE_C_COMPILER=' .. g_cmake_c_compiler,
-    }
-    return os.execute(to_cmd(setup_cmd))
+local function build()
+    print('@build(' .. g_setup .. ')' )
+    return os.execute(g_setup .. ' --build ' .. g_setup_build_dir)
 end
-function build()
-    print('@build(cmake)')
-    build_cmd = {
-        'cmake',
-        '--build ./build/',
-    }
-    return os.execute(to_cmd(build_cmd))
+local function run()
+    print('@run(' .. g_executable .. ' ' .. table.concat(g_executable_args, ' ') ..  ')')
+    return os.execute('start cmd /C ' .. g_setup_build_dir .. g_executable .. ' ' .. table.concat(g_executable_args, ' '))
 end
-function run()
-    print('@run(' .. g_executable .. ' ' .. to_cmd(g_executable_args) .. ')')
-    return os.execute(g_executable .. ' ' .. to_cmd(g_executable_args))
-end
--- Handle Args -----------------------------------------------------------------
+
+-- script --
 print('')
-if in_args('--release') then
-    g_build_type = 'Release'
-end
-if in_args('--debug') then
-    g_build_type = 'Debug'
-end
-if in_args('--run-only') then
-    if not run() then return end
-    return
-end
-if in_args('--run') then
-    if not setup() then return end
-    if not build() then return end
-    if not run() then return end
-    return
-end
-if in_args('--build') then
-    if not setup() then return end
-    if not build() then return end
-    return
-end
-if in_args('--setup') then
-    if not setup() then return end
-    return
-end
-if not setup() then return end
-if not build() then return end
-if not run() then return end
--- End -------------------------------------------------------------------------
+if in_args('--debug') then g_setup_build_type = 'Debug' end
+if in_args('--run') then if not run() then print('Exit with ERROR!') return end return end
+if not setup() or in_args('--setup') then return end
+if not build() or in_args('--build') then return end
+if not run() then print('Exit with ERROR!') end
